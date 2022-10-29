90fd54c6617e077516775d8915e2d08847c9a4d7
Meriç Enes K <merichrocks@icloud.com>
2022-10-29 12:09:32 +0300

    🤔 Vercel Adapter Integrated

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import {mdsvex} from 'mdsvex';
 import remarkGithub from 'remark-github';
@@ -43,7 +43,21 @@ const config = {
         })
     ],
     kit: {
-        adapter: adapter()
+        // default options are shown
+        adapter: vercel({
+            // if true, will deploy the app using edge functions
+            // (https://vercel.com/docs/concepts/functions/edge-functions)
+            // rather than serverless functions
+            edge: false,
+
+            // an array of dependencies that esbuild should treat
+            // as external when bundling functions
+            external: [],
+
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app
+            split: false
+        })
     }
 };
 
